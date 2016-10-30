cfdb54b7576f315d6e5f61c882df6d784c053f9a
Jaspreet Singh <jaashy.singh@gmail.com>
2016-10-30 10:39:54 +0100

    update

--- app.js
+++ app.js
@@ -1,53 +1,26 @@
-/**
- * Module dependencies.
- */
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-var express = require('express')
-  , ejs= require('ejs');
-
-/**
- * App.
- */
-
-var app = express.createServer();
-
-/**
- * App configuration.
- */
-
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.set('view engine', 'ejs');
-  app.set("view options", { layout: false });
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
 
-/**
- * App routes.
- */
+// Routing
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.render('index', { layout: false });
-});
+	app.set('view engine', 'ejs');
+	app.set("view options", { layout: false });
+	app.get('/', function (req, res) {
+	  res.render('index', { layout: false });
+	});
 
-/**
- * App listen.
- */
 
- var port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-  var addr = app.address();
-  console.log('   app listening on http://' + addr.address + ':' + addr.port);
-});
-
-/**
- * Socket.IO server (single process only)
- */
-
-var io = require('socket.io').listen(app)
-  , nicknames = {};
+var nicknames = {};
 
   io.sockets.on('connection', function (socket) {
     socket.on('user message', function (msg) {
